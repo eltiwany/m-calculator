09fb6dbb3c65da95d03e83a633ddd82fb23449f8
Ali Saleh <eltiwany@icloud.com>
2021-03-11 09:59:42 +0300

    Created contributersActivity to appreciate them

---
+++ app/src/main/java/com/nafuutronics/mcalculator/contributersActivity.java
@@ -0,0 +1,14 @@
+package com.nafuutronics.mcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class contributersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contributers);
+    }
+}
